2a4c8c4285f74aed8091138942a110ad7ee04c65
Firas Abd Alrahman <firas@nilogy.com>
2022-03-11 22:24:17 +0300

    create build file

--- mysqlcnfbuilder/build.sh
+++ mysqlcnfbuilder/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 rm -rf ../assets ../canvaskit ../icons/ ../favicon.png ../flutter_service_worker.js ../index.html ../main.dart.js ../manifest.json ../version.json
 flutter build web --release
-cp build/web/* ../
+cp -r build/web/* ../
