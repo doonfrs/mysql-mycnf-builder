fceddada1265259515a3a0e96727a5cd50d541ac
Firas Abd Alrahman <firas@nilogy.com>
2022-03-11 22:27:29 +0300

    move web pages to docs folder

--- mysqlcnfbuilder/build.sh
+++ mysqlcnfbuilder/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf ../assets ../canvaskit ../icons/ ../favicon.png ../flutter_service_worker.js ../index.html ../main.dart.js ../manifest.json ../version.json
 flutter build web --release
-cp -r build/web/* ../
+cp -r build/web/* ../docs/
