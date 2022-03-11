7750d04cd97934be4edc69b098dc159a2c277ad8
Firas Abd Alrahman <firas@nilogy.com>
2022-03-11 22:43:25 +0300

    remove base href

--- mysqlcnfbuilder/build.sh
+++ mysqlcnfbuilder/build.sh
@@ -2,3 +2,4 @@
 rm -rf ../assets ../canvaskit ../icons/ ../favicon.png ../flutter_service_worker.js ../index.html ../main.dart.js ../manifest.json ../version.json
 flutter build web --release
 cp -r build/web/* ../docs/
+sed -i '/<base href="\/">/d' ../docs/index.html
